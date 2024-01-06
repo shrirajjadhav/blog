90ca0dc84447ca66c188f1ec538cee3a2db63ad3
Shriraj Jadhav <git@shriraj.dev>
2024-01-06 15:43:02 +0530

    Edited 'Constants.ts'
     - edited blog title
     - edited blog descriptioin

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Shriraj.dev(blog)';
+export const SITE_DESCRIPTION = 'Welcome to my Dev blog where I write about anything tech-worthy 😁';
